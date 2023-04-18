8f9f89b7b90d040ed49e95a84260e6982275d2f4
mikerozendo <mikerozendo@gmail.com>
2023-04-18 12:53:42 -0300

    Adicionando projeto web

--- src/MikeCars.Domain/Entities/Operacao.cs
+++ src/MikeCars.Domain/Entities/Operacao.cs
@@ -8,7 +8,7 @@ public abstract class Operacao : Base
     public EnumSituacaoOperacao EnumSituacaoOperacao { get; set; }
     public Operador Operador { get; protected set; }
     public Agente OperadorExterno { get; protected set; }
-    public DateTime Data { get; set; }
+    public DateTime Lancamento { get; set; }
     public DateTime UltimaAlteracao { get; set; }
     public decimal ValorTotal { get; private set; }
     public IEnumerable<Ativo> Ativos { get; set; } = new List<Ativo>();
