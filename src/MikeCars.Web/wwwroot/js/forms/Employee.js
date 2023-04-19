4899bc9f2ddc1fbceba5c852aed937c4b16de853
mikerozendo <mikerozendo@gmail.com>
2023-04-19 01:48:07 -0300

    ok

--- src/MikeCars.Web/wwwroot/js/forms/Employee.js
+++ src/MikeCars.Web/wwwroot/js/forms/Employee.js
@@ -1,3 +1,13 @@
-export function uploadImage() {
-
+//export function uploadImage() {
+//    let buttonUpload = document.getElementById("btn-upload-img");
+//}
+function triggerBtnUpload() {
+    document
+        .getElementById("btn-upload-img")
+        .addEventListener("click", function (e) {
+            debugger;
+            e.preventDefault();
+            let inputFileBtn = document.getElementById("btn-img-file");
+            inputFileBtn.click();      
+        });
 }
--- src/MikeCars.Web/wwwroot/js/site.js
+++ src/MikeCars.Web/wwwroot/js/site.js
@@ -6,12 +6,17 @@
     }
 }
 
-//function showInvisleElementsByStep(inputId) {
-//    let elements = document.getElementById(inputId);
-
-//    let elements = document.querySelectorAll("not-visible");
-
-//    for (i = 0; i < elements.length; i++) {
-//        elements[i].classList.remove("not-visible");
-//    }
-//}
+function getAdressByZipCode(zipCode) {
+    if (zipCode.length == 8) {
+        let viaCepResponse = await fetch("https://viacep.com.br/ws/" + zipCode + "/json/");
+        let jsonData = await viaCepResponse.json();
+        console.log(jsonData);
+        return await jsonData;
+    }
+    else {
+        document
+            .getElementById("spn-zip-code-error")
+            .classList
+            .remove("not-visible");
+    }
+}
