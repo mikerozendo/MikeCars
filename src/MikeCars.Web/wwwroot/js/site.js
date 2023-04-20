e435dddbb84e51b5ecb349d257e02484f41291fe
mikerozendo <mikerozendo@gmail.com>
2023-04-20 16:19:17 -0300

    adding a new partial view to reutilize toast

--- src/MikeCars.Web/wwwroot/js/site.js
+++ src/MikeCars.Web/wwwroot/js/site.js
@@ -1,10 +1,9 @@
 ﻿function showToast() {
     var toastElList = [].slice.call(document.querySelectorAll('.toast'))
     var toastList = toastElList.map(function (toastEl) {
-        // Creates an array of toasts (it only initializes them)
-        return new bootstrap.Toast(toastEl) // No need for options; use the default options
+        return new bootstrap.Toast(toastEl)
     });
-    toastList.forEach(toast => toast.show()); // This show them
+    toastList.forEach(toast => toast.show());
 }
 
 function buildToastMessase(message) {
@@ -70,39 +69,19 @@ function filterOnTable(filterElement) {
     }
 }
 
+//function showInvisibleElements() {
+//    document.querySelectorAll("not-visible").forEach(
+//        function (element, i) {
+//            debugger;
+//            element[i].classList.remove("not-visible");
+//            console.log('[forEach]', nome, i);
+//        });
 
-function showInvisibleElements() {
-    document.querySelectorAll("not-visible").forEach(
-        function (element, i) {
-            debugger;
-            element[i].classList.remove("not-visible");
-            console.log('[forEach]', nome, i);
-        });
-
-    for (i = 0; i < elements.length; i++) {
-        elements[i].classList.remove("not-visible");
-    }
-}
-
-//function getAdressByZipCode(zipCode) {
-//    if (zipCode.length == 8) {
-//        let viaCepResponse = await fetch("https://viacep.com.br/ws/" + zipCode + "/json/");
-//        let jsonData = await viaCepResponse.json();
-//        console.log(jsonData);
-//        return await jsonData;
-//    }
-//    else {
-//        document
-//            .getElementById("spn-zip-code-error")
-//            .classList
-//            .remove("not-visible");
+//    for (i = 0; i < elements.length; i++) {
+//        elements[i].classList.remove("not-visible");
 //    }
 //}
 
-function setTwoNumberDecimal(event) {
-    this.value = parseFloat(this.value).toFixed(2);
-}
-
-//function editForm() {
-
+//function setTwoNumberDecimal(event) {
+//    this.value = parseFloat(this.value).toFixed(2);
 //}
