be0cafc3eda18fb5e65c6176d09192ad69554257
mikerozendo <mikerozendo@gmail.com>
2023-04-20 00:40:38 -0300

    adding a toast to notify user mistakes + finishin function to get adress based in zipcode + finishing function to validate documentnumber digits

--- src/MikeCars.Web/wwwroot/js/site.js
+++ src/MikeCars.Web/wwwroot/js/site.js
@@ -1,4 +1,14 @@
-﻿function showInvisibleElements() {
+﻿function showToast() {
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+    var toastList = toastElList.map(function (toastEl) {
+        // Creates an array of toasts (it only initializes them)
+        return new bootstrap.Toast(toastEl) // No need for options; use the default options
+    });
+    toastList.forEach(toast => toast.show()); // This show them
+}
+
+
+function showInvisibleElements() {
     document.querySelectorAll("not-visible").forEach(
         function (element, i) {
             debugger;
